970fe427e797808aba39e70b95470f23b1eb42e6
Matthew Macdonald-Wallace <matt@doics.co>
2025-07-29 14:21:34 +0100

    Last few changes to make sure we keep away from deprecated arduinojson functions

--- include/OtelLogger.h
+++ include/OtelLogger.h
@@ -1,13 +1,13 @@
 #ifndef OTEL_LOGGER_H
 #define OTEL_LOGGER_H
 
-#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
 #include "OtelDefaults.h"
 #include "OtelSender.h"
 
 namespace OTel {
 
-// This is your one-and-only resource config singleton
+// Your one-and-only resource config singleton
 static inline OTelResourceConfig& defaultResource() {
   static OTelResourceConfig rc;
   return rc;
@@ -31,13 +31,13 @@ public:
 
   // Core log emitter
   static void log(const char* severity, const String& message) {
-    // adjust capacity as needed
-    DynamicJsonDocument doc(1024);
+    // In ArduinoJson 7+, JsonDocument grows as needed (heap‐based)
+    JsonDocument doc;
 
     // Top-level resourceLogs array
     JsonObject resourceLog = doc["resourceLogs"].add<JsonObject>();
 
-    // Populate resource attributes from the same singleton you set in begin()
+    // Populate resource attributes from the singleton you set in begin()
     JsonObject resource = resourceLog["resource"].to<JsonObject>();
     defaultResource().addResourceAttributes(resource);
 
@@ -48,18 +48,18 @@ public:
     scope["version"]    = "0.1.0";
 
     // the log record itself
-    JsonObject logEntry            = scopeLog["logRecords"].add<JsonObject>();
-    logEntry["timeUnixNano"]       = nowUnixNano();
-    JsonObject body                = logEntry["body"].to<JsonObject>();
-    body["stringValue"]            = message;
-    logEntry["severityText"]       = severity;
+    JsonObject logEntry      = scopeLog["logRecords"].add<JsonObject>();
+    logEntry["timeUnixNano"] = nowUnixNano();
+    JsonObject body          = logEntry["body"].to<JsonObject>();
+    body["stringValue"]      = message;
+    logEntry["severityText"] = severity;
 
     // send to OTLP HTTP exporter
     OTelSender::sendJson("/v1/logs", doc);
   }
 
-  static void logInfo(const char* msg)    { log("INFO", String(msg)); }
-  static void logInfo(const String& msg)  { log("INFO", msg); }
+  static void logInfo(const char* msg)   { log("INFO", String(msg)); }
+  static void logInfo(const String& msg) { log("INFO", msg); }
 };
 
 } // namespace OTel
